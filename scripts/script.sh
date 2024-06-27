754df83f18f3da1983bd8d8791a41e53fe53728a
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2024-06-27 12:03:58 -0400

    Add output name for slurm jobs

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-mbowling
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=8G      
 #SBATCH --time=2:59:00
-
+#SBATCH --output=qlearning.out
 
 # SOCKS5 Proxy
 if [ "$SLURM_TMPDIR" != "" ]; then
@@ -13,7 +13,7 @@ if [ "$SLURM_TMPDIR" != "" ]; then
 fi
 
 # Setup Modules
-module load python/3.10 gcc/11.3.0 cuda/11.8.0
+module load python/3.10
 
 # Setup Python Environment
 cd $SLURM_TMPDIR
@@ -38,6 +38,6 @@ mkdir results
 
 #wandb variables
 export WANDB_API_KEY=c661d4027cae102ac37a9dd80433c1648bab0e56
-export SWEEP_ID=$(wandb sweep tabular/config/qlearning_config.yml --project qlearning)
-wandb agent $SWEEPID
+export SWEEP_ID=$(wandb sweep tabular/config/qlearning_config.yaml --project tile-coding-exploration)
+wandb agent --project "tile-coding-explroation"  $SWEEPID
 
